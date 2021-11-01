4c8b355fa060b8080d6751eeb8862a09a754b9d7
SeongMin-K <eo1tks3@naver.com>
2021-11-01 19:45:39 +0900

    Initial Commit

---
+++ Panmodal_Tutorial/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Panmodal_Tutorial
+//
+//  Created by SeongMinK on 2021/11/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
